486c2b0437b2b9d0c42b902e9f65787ffcc72235
emWright94 <emwright94@gmail.com>
2025-01-19 16:52:03 -0500

    added weather data in js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,61 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let temperature = response.data.temperature.current;
+  let humidityElement = document.querySelector("#humidity-percentage");
+  let windSpeedElement = document.querySelector("#wind-mph");
+  let windSpeed = response.data.wind.speed;
+  let dayTimeElement = document.querySelector("#day-time");
+  let date = new Date(response.data.time * 1000);
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windSpeedElement.innerHTML = Math.round(windSpeed);
+  dayTimeElement.innerHTML = formatDayTime(date);
+
+  console.log(response.data);
+}
+
+function formatDayTime(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "fa01ob92afac42305bb6e069bt2408b4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input-city");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Greenwich");
+
+// wind-mph
